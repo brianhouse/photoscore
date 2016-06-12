e6085f8fb9dbe9248b9bacad1c0ceb061a4238c0
brianhouse <brian.house@gmail.com>
2016-06-12 15:41:05 -0600

    queues

--- play.py
+++ play.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-import sys, json, time, threading
+import sys, json, time, threading, queue
 from housepy import config, log, util, midi
 from score import Note
 
 midi.log_midi = True
 
-DURATION = 1 * 60.0 # minutes
+DURATION = 1 * 10.0 # minutes
 MIN_VELOCITY = 0.5
 
 midi_out = midi.MidiOut(1)
@@ -17,34 +17,36 @@ class Player(threading.Thread):
         threading.Thread.__init__(self)
         self.daemon = False if __name__ == "__main__" else True
         self.playing = False
-        self.notes = None
+        self.queue = queue.Queue()
+        self.start()
 
     def play(self, notes):
         if self.playing:
             log.warning("Already playing")
             return  
-        self.notes = notes      
-        self.start()
+        self.queue.put(notes)
 
     def run(self):
-        log.info("Playing...")
-        self.playing = True
-        note_ons = [(note.on * DURATION, int(note.channel), int(note.pitch), int(127 * ((note.velocity * MIN_VELOCITY) + MIN_VELOCITY))) for note in self.notes]
-        note_offs = [(note.off * DURATION, int(note.channel), int(note.pitch), 0) for note in self.notes]
-        notes = (note_ons + note_offs)
-        notes.sort(key=lambda n: n[0])
-        start_t = time.time()
-        n = 0
         while True:
-            t = time.time() - start_t
-            if t > notes[n][0]:
-                midi_out.send_note(notes[n][1], notes[n][2], notes[n][3])
-                n += 1
-                if n == len(notes):
-                    break
-            time.sleep(0.01)
-        self.playing = False
-        log.info("--> done")
+            notes = self.queue.get()
+            log.info("Playing...")
+            self.playing = True
+            note_ons = [(note.on * DURATION, int(note.channel), int(note.pitch), int(127 * ((note.velocity * MIN_VELOCITY) + MIN_VELOCITY))) for note in notes]
+            note_offs = [(note.off * DURATION, int(note.channel), int(note.pitch), 0) for note in notes]
+            notes = (note_ons + note_offs)
+            notes.sort(key=lambda n: n[0])
+            start_t = time.time()
+            n = 0
+            while True:
+                t = time.time() - start_t
+                if t > notes[n][0]:
+                    midi_out.send_note(notes[n][1], notes[n][2], notes[n][3])
+                    n += 1
+                    if n == len(notes):
+                        break
+                time.sleep(0.01)
+            self.playing = False
+            log.info("--> done")
 
 
 if __name__ == "__main__":
